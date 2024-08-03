437d6c60e9d288fd210e7023f6c39742159a32fe
caiocardos <84928031+caiocardos@users.noreply.github.com>
2024-08-03 20:15:48 +0000

    atv_04_inicio

--- atv 04/Main.java
+++ atv 04/Main.java
@@ -1,3 +1,146 @@
 public class Main {
+
+    public static void imprimeMatriz(double[][] matriz){
+        for(int i=0; i<matriz.length; i++){
+            for(int j=0; j<matriz[0].length; j++){
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }    
+
+    public static void testToString (){
+        Matrix a = new Matrix(new double[][]{
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        });
+        Matrix b = new Matrix(new double[][]{
+            {12, 10, 8},
+            {7, 6, 5},
+            {4, 3, 2}
+        });
     
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+    public static void testToArray (){
+        Matrix a = new Matrix(new double[][]{
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        });
+        double[][] cells = a.toArray();
+        System.out.println("Convertendo Matrix em Array:");
+        imprimeMatriz(cells);       
+
+        for(int i=0; i<cells.length; i++){
+            for(int j=0; j<cells[0].length; j++){
+                cells[i][j] = 1;
+            }
+        }
+        System.out.println("Alterando o Array recebido do objeto Matrix:");
+        imprimeMatriz(cells);
+        System.out.println("Alteracao nao propaga no objeto Matrix:");
+        System.out.println(a);
+    }
+
+    public static void testRowColumnsGetAt(){
+        Matrix a = new Matrix(new double[][]{
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        });
+        Matrix b = new Matrix(new double[][]{
+            {12, 10, 8},
+            {7, 6, 5},
+        });
+
+        System.out.println("O Objeto a eh uma matriz " + a.getRows() + "x" + a.getColumns());
+        System.out.println("O Objeto b eh uma matriz " + b.getRows() + "x" + b.getColumns());
+
+        System.out.println("Valor da matriz a em [1][1] eh: " + a.getAt(1, 1));
+        //System.out.println("Valor da matriz a em [3][3] (invalida) eh: " + a.getAt(3, 3));
+
+    }
+
+    public static void testPlusMinus(){
+        Matrix a = new Matrix(new double[][]{
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        });
+        Matrix b = new Matrix(new double[][]{
+            {12, 10, 8},
+            {7, 6, 5},
+            {4, 3, 2}
+        });
+
+        Matrix c = new Matrix(new double[][]{
+            {4, 8, 12},
+            {3, 6, 9},
+            {1, 2, 3},
+            {5, 10, 15}
+        });
+
+        System.out.println("Matriz a: ");
+        System.out.println(a);
+        System.out.println("Matriz b: ");
+        System.out.println(b);
+        System.out.println("Matriz a + Matriz b: ");
+        System.out.println(a.plus(b));
+        // System.out.println("Matriz a + Matriz c: (INCOMPATIVEIS)");
+        // System.out.println(a.plus(c));
+        System.out.println("Matriz a - Matriz b: ");
+        System.out.println(a.minus(b));
+        System.out.println("Matriz a - Matriz c: (INCOMPATIVEIS)");
+        System.out.println(a.minus(c));
+    }
+
+    public static void testTimes(){
+        Matrix a = new Matrix(new double[][]{
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        });
+        Matrix b = new Matrix(new double[][]{
+            {12, 10, 8},
+            {7, 6, 5},
+            {4, 3, 2}
+        });
+
+        Matrix c = new Matrix(new double[][]{
+            {4, 8, 12, 6},
+            {3, 6, 9, 8},
+            {1, 2, 3, 5}
+        });
+
+        System.out.println("Matriz a: ");
+        System.out.println(a);
+        System.out.println("Matriz b: ");
+        System.out.println(b);
+        System.out.println("Matriz axb: ");
+        System.out.println(a.times(b));
+        System.out.println("Matriz 2*a: ");
+        System.out.println(a.times(2));
+        System.out.println("Matriz axc: ");
+        System.out.println(a.times(c));
+        //System.out.println("Matriz cxa: (INCOMPATIVEIS) ");
+        //System.out.println(c.times(a));
+
+
+    }
+    public static void main(String[] args) {
+        //testToString();
+        //testToArray();
+        //testRowColumnsGetAt();
+        //testPlusMinus();
+        testTimes();
+        
+        // System.out.println(a.isSquare());
+        // System.out.println(b.isSymmetric());
+        // System.out.println(a.getTranspose());
+
+    }
 }
--- atv 04/Matrix.java
+++ atv 04/Matrix.java
@@ -1,3 +1,112 @@
 public class Matrix {
+    private final double[][] cells;
     
+    public Matrix(double[][] c){
+        cells = new double[c.length][c[0].length];
+        
+        for (int i = 0; i < c.length; i++){
+            for(int j = 0; j< c[0].length; j++){
+                cells[i][j] = c[i][j];
+            }
+        }
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder f = new StringBuilder();
+        for (int i = 0; i < cells.length; i++){
+            for(int j = 0; j < cells[0].length; j++){
+                f.append(String.format("%.6f  ",cells[i][j]));
+            }
+            f.append("\n");
+            for(int k = 0; k<=i; k++){
+                f.append("    ");
+            }            
+        }
+        
+        return f.toString();
+    }
+
+    public double[][] toArray(){
+        double[][] array = new double[cells.length][cells[0].length];
+        for (int i = 0; i < cells.length; i++){
+            for(int j = 0; j< cells[0].length; j++){
+                array[i][j] = cells[i][j];
+            }
+        }
+        return array;
+    }
+    
+    public int getRows(){
+        return cells.length;
+    }
+
+    public int getColumns(){
+        return cells[0].length;
+    }
+
+    public double getAt(int row, int column){
+        if (row < 0 || column < 0 || row >= cells.length || column >= cells[0].length){
+            throw new IllegalArgumentException("Posicao invalida!");
+        }
+        return cells[row][column];
+    }
+
+    public Matrix plus(Matrix m){
+        if (cells.length != m.getRows() || cells[0].length != m.getColumns()){
+            throw new IllegalArgumentException("Matrizes com dimensoes incompativeis!");
+        }
+        double[][] sum = new double[cells.length][cells[0].length];
+        for (int i = 0; i < cells.length; i++){
+            for (int j = 0; j < cells[0].length; j++){
+                sum[i][j] = cells[i][j] + m.getAt(i, j);                
+            }
+        }
+        Matrix res = new Matrix(sum);
+        return res;
+    }
+
+    public Matrix minus(Matrix m){
+        if (cells.length != m.getRows() || cells[0].length != m.getColumns()){
+            throw new IllegalArgumentException("Matrizes com dimensoes incompativeis!");
+        }
+        double[][] sub = new double[cells.length][cells[0].length];
+        for (int i = 0; i < cells.length; i++){
+            for (int j = 0; j < cells[0].length; j++){
+                sub[i][j] = cells[i][j] - m.getAt(i, j);                
+            }
+        }
+        Matrix res = new Matrix(sub);
+        return res;
+    }
+
+    public Matrix times (Matrix a){
+        if (cells[0].length != a.getRows()){
+            throw new IllegalArgumentException("Matrizes com dimensoes incompativeis!");
+        }
+        double[][] mul = new double[cells.length][a.getColumns()];
+        for (int i = 0; i < cells.length; i++){
+            for (int j = 0; j < a.getColumns(); j++){
+                for (int k = 0; k < cells[0].length; k++){
+                    mul[i][j] += (cells[i][k]* a.getAt(k, j));
+                }                
+            }
+        }
+        Matrix res = new Matrix(mul);
+        return res;
+    }
+
+    public Matrix times (double a){
+        double[][] mul = new double[cells.length][cells[0].length];
+        for (int i = 0; i < cells.length; i++){
+            for (int j = 0; j < cells[0].length; j++){
+                mul[i][j] = cells[i][j] * a;                
+            }
+        }
+        Matrix res = new Matrix(mul);
+        return res;
+    }
 }
+
+
+
